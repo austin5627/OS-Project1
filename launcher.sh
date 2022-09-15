7b6e9542d99c3771e4b623384f039ebdedbe6009
ethanwcooper <ethanwcooper@outlook.com>
2022-09-14 19:09:00 -0500

    Adding the message classes from the SCTP example code

---
+++ launcher.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Change this to your netid
+netid=ewc180001
+
+# Root directory of your project
+PROJDIR=$HOME/cs6378/proj0
+
+# Directory where the config file is located on your local system
+CONFIGLOCAL=$HOME/launch/config.txt
+
+# Directory your java classes are in
+BINDIR=$PROJDIR
+
+# Your main project class
+PROG=Node
+
+n=0
+
+cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
+(
+    read i
+    echo $i
+    while [[ $n -lt $i ]]
+    do
+    	read line
+    	node=$( echo $line | awk '{ print $1 }' )
+        host=$( echo $line | awk '{ print $2 }' )
+        port=$( echo $line | awk '{ print $3 }' )
+	
+        xterm -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $node $port; exec bash" &
+
+        n=$(( n + 1 ))
+    done
+)
