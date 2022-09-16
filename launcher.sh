7cfd8e2b6225023ecc86ee7315bee8f0726fd528
ethanwcooper <ethanwcooper@outlook.com>
2022-09-16 18:01:38 -0500

    Changes by Austin

--- launcher.sh
+++ launcher.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Change this to your netid
-netid=ewc180001
+export netid=ewc180001
 
 # Root directory of your project
-PROJDIR=$HOME/cs6378/proj1
+export PROJDIR=$HOME/cs6378/proj1
 
 # Directory where the config file is located on your local system
-CONFIGLOCAL=$HOME/launch/config.txt
+export CONFIGLOCAL=$HOME/launch/config.txt
 
 # Directory your java classes are in
-BINDIR=$PROJDIR
+export BINDIR=$PROJDIR
 
 # Your main project class
-PROG=Launcher
+export PROG=Launcher
