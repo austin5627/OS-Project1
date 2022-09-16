036510dbb7d00f7d83ec1ea827714dd683b98e72
ethanwcooper <ethanwcooper@outlook.com>
2022-09-16 18:35:47 -0500

    Launcher

---
+++ out/production/Project1/README.txt
@@ -0,0 +1,1 @@
+Just a file for any compilation/running instructions
---
+++ out/production/Project1/launcher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change this to your netid
+export netid=ewc180001
+
+# Root directory of your project
+export PROJDIR=$HOME/cs6378/proj1
+
+# Directory where the config file is located on your local system
+export CONFIGLOCAL=$HOME/launch/config.txt
+
+# Directory your java classes are in
+export BINDIR=$PROJDIR
+
+# Your main project class
+export PROG=Launcher
