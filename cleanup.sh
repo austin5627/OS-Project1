1745c69d708d6c929eb6df65863df48922f0282f
ethanwcooper <ethanwcooper@outlook.com>
2022-09-18 14:59:50 -0500

    Launcher

--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,7 @@ netid=ewc180001
 
 #
 # Root directory of your project
-PROJDIR=$HOME/cs6378/proj0
+PROJDIR=$HOME/cs6378/proj1/cs6378proj1
 
 #
 # Directory where the config file is located on your local system
